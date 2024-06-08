a5f10cd055541d6bacdd4798ab25d485c6b63aaa
xzralgen <113490963+xzralgen@users.noreply.github.com>
2024-06-08 21:09:35 +0800

    Update DE_Zoomcamp.py

--- dezoomcamp/DE_Zoomcamp.py
+++ dezoomcamp/DE_Zoomcamp.py
@@ -19,26 +19,6 @@ show_pages(
         Page("dezoomcamp/2024_cohort/Workshop_2_Stream_Processing_with_SQL.py", "Workshop 2 Stream Processing with SQL", "🛠️", in_section=True),
         Page("dezoomcamp/2024_cohort/Module_6_Stream_Processing.py", "Module 6 Stream Processing", "6️⃣", in_section=True),
         Page("dezoomcamp/2024_cohort/Course_Project.py", "Course Project", "🏆", in_section=True),
-
-        # 2023 Content
-        Section("DE Zoomcamp 2023", "👨‍🔧"),
-        Page("dezoomcamp/2023_cohort/Course_Overview.py", "Course Overview", "📚", in_section=True),
-        Page("dezoomcamp/2023_cohort/Week_1_Introduction_&_Prerequisites.py", "Week 1 Introduction & Prerequisites", "1️⃣", in_section=True),
-        Page("dezoomcamp/2023_cohort/Week_2_Workflow_Orchestration.py", "Week 2 Workflow Orchestration", "2️⃣", in_section=True),
-        Page("dezoomcamp/2023_cohort/Week_3_Data_Warehouse.py", "Week 3 Data Warehouse", "3️⃣", in_section=True),
-        Page("dezoomcamp/2023_cohort/Week_4_Analytics_Engineering.py", "Week 4 Analytics Engineering", "4️⃣", in_section=True),
-        Page("dezoomcamp/2023_cohort/Week_5_Batch_Processing.py", "Week 5 Batch Processing", "5️⃣", in_section=True),
-        Page("dezoomcamp/2023_cohort/Week_6_Stream_Processing.py", "Week 6 Stream Processing", "6️⃣", in_section=True),
-        Page("dezoomcamp/2023_cohort/Week_7_Project.py", "Week 7 Project", "7️⃣", in_section=True),
-        Page("dezoomcamp/2023_cohort/Homework_Quizzes.py", "Homework Quizzes", "📝", in_section=True),
-        
-        Page("dezoomcamp/Datasets.py", "Datasets", icon="💾", in_section=False),
-        Page("dezoomcamp/Certificate.py", "Certificate", "📜", in_section=False),
-        Page("dezoomcamp/FAQ.py", "FAQ", "❔", in_section=False),
-        Page("dezoomcamp/Contact.py", "Contact", icon="📩", in_section=False),   
-        Page("dezoomcamp/Contact_thanks.py", "Thank you", icon="💌"),   
-        Page("dezoomcamp/About.py", "About", icon="🖼️", in_section=False) 
-    ]
 )
 
 hide_pages(["Thank you"])
